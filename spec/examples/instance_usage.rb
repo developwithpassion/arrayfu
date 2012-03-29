be91c24f436828a01692507f4385930cb23fb373
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-03-28 20:52:54 -0600

    Completed the examples

--- spec/examples/instance_usage.rb
+++ spec/examples/instance_usage.rb
@@ -34,63 +34,77 @@ end
 
 example 'Allow the array to have a read and write accessor' do
   class SomeClass
-    array :names do|a|
-      a.read_and_write
+    def initialize
+      array :names do|a|
+        a.read_and_write
+      end
     end
   end
-  SomeClass.names.should_not be_nil
+  SomeClass.new.names.should_not be_nil
 end
 
 example 'Add a mutator method to the class that stores the array' do
   class SomeClass
-    array :names do|a|
-      a.mutator :add_item
+    def initialize
+      array :names do|a|
+        a.mutator :add_item
+      end
     end
   end
 
-  SomeClass.add_item("JP")
-  SomeClass.names.count.should == 1
+  items = SomeClass.new
+  items.add_item("JP")
+  items.names.count.should == 1
 end
 
 example 'Add multiple mutators to the class that stores the array' do
   class SomeClass
-    array :names do|a|
-      a.mutator :add_item,:add_it,:push_it
+    def initialize
+      array :names do|a|
+        a.mutator :add_item,:add_it,:push_it
+      end
     end
   end
 
-  SomeClass.add_item("JP")
-  SomeClass.add_it("JP")
-  SomeClass.push_it("JP")
-  SomeClass.names.count.should == 3
+  items = SomeClass.new
+  items.add_item("JP")
+  items.add_it("JP")
+  items.push_it("JP")
+  items.names.count.should == 3
 end
 
 example 'Add a mutator that ignores addition' do
   class SomeClass
-    array :names do|a|
-      a.mutator :add_item do|item|
+    def initialize
+      array :names do|a|
+        a.mutator :add_item do|item|
+        end
       end
     end
   end
 
-  SomeClass.add_item("JP")
-  SomeClass.names.count.should == 0
+  items = SomeClass.new
+  items.add_item("JP")
+  items.names.count.should == 0
 end
 
 example 'Add a mutator that does other custom logic as well as addition' do
   class SomeClass
-    array :secondary
-    array :names do|a|
-      a.mutator :add_item do|item|
-        @secondary.push item
-        @names.push item
+    def initialize
+      array :secondary
+      array :names do|a|
+        a.mutator :add_item do|item|
+          @secondary.push item
+          @names.push item
+        end
       end
     end
   end
 
-  SomeClass.add_item("JP")
-  SomeClass.names.count.should == 1
-  SomeClass.secondary.count.should == 1
+  items = SomeClass.new
+  items.add_item("JP")
+  items.names.count.should == 1
+  items.secondary.count.should == 1
 end
 
 example 'Add a singular constraint and failure condition to each of the mutators' do
@@ -106,20 +120,23 @@ example 'Add a singular constraint and failure condition to each of the mutators
   class CriteriaViolation
     include Singleton
     def run(description,value)
-      
+
     end
   end
 
   class SomeClass
-    array :names do|a|
-      a.mutator :add_item,:add_it
-      a.new_item_must NotBeJP.instance,CriteriaViolation.instance
+    def initialize
+      array :names do|a|
+        a.mutator :add_item,:add_it
+        a.new_item_must NotBeJP.instance,CriteriaViolation.instance
+      end
     end
   end
 
-  SomeClass.add_item("JP")
-  SomeClass.add_it("JP")
-  SomeClass.names.count.should == 0
+  items = SomeClass.new
+  items.add_item("JP")
+  items.add_it("JP")
+  items.names.count.should == 0
 end
 
 example 'Add multiple constraints and a failure condition to each of the mutators' do
@@ -144,22 +161,25 @@ example 'Add multiple constraints and a failure condition to each of the mutator
   class CriteriaViolation
     include Singleton
     def run(description,value)
-      
+
     end
   end
 
   class SomeClass
-    array :names do|a|
-      a.mutator :add_item,:add_it
-      a.new_item_must NotBeJP.instance,CriteriaViolation.instance
-      a.new_item_must NotBeNil.instance,CriteriaViolation.instance
+    def initialize
+      array :names do|a|
+        a.mutator :add_item,:add_it
+        a.new_item_must NotBeJP.instance,CriteriaViolation.instance
+        a.new_item_must NotBeNil.instance,CriteriaViolation.instance
+      end
     end
   end
 
-  SomeClass.add_item("JP")
-  SomeClass.add_it("JP")
-  SomeClass.add_item(nil)
-  SomeClass.names.count.should == 0
+  items = SomeClass.new
+  items.add_item("JP")
+  items.add_it("JP")
+  items.add_item(nil)
+  items.names.count.should == 0
 end
 
 example 'Add an explicit processing visitor to the array' do
@@ -176,14 +196,17 @@ example 'Add an explicit processing visitor to the array' do
   end
 
   class SomeClass
-    array :names do|a|
-      a.mutator :add_item
-      a.process_using :display_all,DisplayItem
+    def initialize
+      array :names do|a|
+        a.mutator :add_item
+        a.process_using :display_all,DisplayItem
+      end
     end
   end
 
-  (1..10).each{|item| SomeClass.add_item(item)}
-  SomeClass.display_all
+  items = SomeClass.new
+  (1..10).each{|item| items.add_item(item)}
+  items.display_all
   DisplayItem.item_count.should == 10
 end
 
@@ -196,9 +219,12 @@ example 'Add an method based processing visitor to the array based on a method t
 
   class SomeClass
     @@items_visited = 0
-    array :names do|a|
-      a.mutator :add_item
-      a.process_using :display_all,:process #the second symbol is the name of a method on an element in the array
+
+    def initialize
+      array :names do|a|
+        a.mutator :add_item
+        a.process_using :display_all,:process #the second symbol is the name of a method on an element in the array
+      end
     end
 
     #the process method of the Item class invokes this method (a little bit roundabout, but it hopefully demonstrates the capability
@@ -210,7 +236,8 @@ example 'Add an method based processing visitor to the array based on a method t
     end
   end
 
-  (1..10).each{|item| SomeClass.add_item(Item.new)}
-  SomeClass.display_all
+  items = SomeClass.new
+  (1..10).each{|item| items.add_item(Item.new)}
+  items.display_all
   SomeClass.number_of_items_visited.should == 10
 end
